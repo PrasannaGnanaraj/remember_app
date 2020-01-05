c1f45a9afc47d4a4dd8568a7a0944bb754d55195
Prasanna Gnanaraj <gnanaprasanna.gnanaraj@gmail.com>
2020-01-05 20:18:38 +0000

    google signin

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script>
   import Header from "./Header.svelte";
   import Notes from "./Notes.svelte";
+  import { onMount } from "svelte";
+
+  const GOOGLE_BUTTON_ID = "google-sign-in-button";
+
+  const onSignIn = googleUser => {
+    const profile = googleUser.getBasicProfile();
+    console.log("Name: " + profile.getName());
+  };
 </script>
 
-<Header />
+<Header>
+  <div class="g-signin2" data-onsuccess="onSignIn" />
+</Header>
 <Notes />
