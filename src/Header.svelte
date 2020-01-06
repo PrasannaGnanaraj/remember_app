976765f80f15a857b29301f97a735072e20113ac
Prasanna Gnanaraj <gnanaprasanna.gnanaraj@gmail.com>
2020-01-06 19:25:10 +0000

    header image

--- src/Header.svelte
+++ src/Header.svelte
@@ -5,5 +5,7 @@
 <div class="bg-black p-4">
   <h1 class="text-white">Remember</h1>
   <slot />
-  <img src={Userimage} class="float-right" alt="User Image" />
+  {#if Userimage}
+    <img src={Userimage} class="float-right" alt="User Image" />
+  {/if}
 </div>
