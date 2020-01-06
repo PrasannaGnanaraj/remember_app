4f59598c5caf386c2d15584d47b1c8e5e4118203
Prasanna Gnanaraj <gnanaprasanna.gnanaraj@gmail.com>
2020-01-06 19:41:52 +0000

    round image

--- src/Header.svelte
+++ src/Header.svelte
@@ -5,7 +5,10 @@
 <div class="bg-black p-4 flex">
   <h1 class="text-white flex-1 w-full my-auto">Remember</h1>
   {#if Userimage}
-    <img src={Userimage} alt="User Image" />
+    <img
+      src={Userimage}
+      class="rounded-full h-16 w-16 flex items-center justify-center"
+      alt="User Image" />
   {:else}
     <slot />
   {/if}
